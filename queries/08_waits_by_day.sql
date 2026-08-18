5eb5b0ae8ee7e4d7c0cd71289782d17fa2bed921
floppyotter <136775387+floppyotter@users.noreply.github.com>
2026-08-17 22:01:00 -0400

    Fix day analysis timezone

--- queries/08_waits_by_day.sql
+++ queries/08_waits_by_day.sql
@@ -1,7 +1,16 @@
 -- Average wait times by day of week
+-- Convert UTC timestamps to Eastern time
+
+WITH local_waits AS (
+    SELECT
+        wait_minutes,
+        datetime(recorded_at, 'localtime') AS local_recorded_at
+    FROM wait_times
+    WHERE wait_minutes IS NOT NULL
+)
 
 SELECT
-    CASE strftime('%w', recorded_at)
+    CASE strftime('%w', local_recorded_at)
         WHEN '0' THEN 'Sunday'
         WHEN '1' THEN 'Monday'
         WHEN '2' THEN 'Tuesday'
@@ -10,11 +19,14 @@ SELECT
         WHEN '5' THEN 'Friday'
         WHEN '6' THEN 'Saturday'
     END AS day_of_week,
+
     ROUND(AVG(wait_minutes), 1) AS avg_wait,
     MIN(wait_minutes) AS min_wait,
     MAX(wait_minutes) AS max_wait,
     COUNT(*) AS observations
-FROM wait_times
-WHERE wait_minutes IS NOT NULL
-GROUP BY strftime('%w', recorded_at)
-ORDER BY CAST(strftime('%w', recorded_at) AS INTEGER);
+
+FROM local_waits
+
+GROUP BY strftime('%w', local_recorded_at)
+
+ORDER BY CAST(strftime('%w', local_recorded_at) AS INTEGER);
